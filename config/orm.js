bdac8e63c1ea476597b203d194d193e81610ddf9
Aaron <aaron@keisler.dev>
2019-08-10 22:57:35 -0400

    add orm functions

---
+++ config/orm.js
@@ -0,0 +1,38 @@
+const connection = require("./connection");
+const { objToSql } = require("../util/helpers.js");
+
+module.exports = {
+  selectAll: (table, callback) => {
+    const queryString = "SELECT * FROM ??";
+    connection.query(queryString, [table], (err, data) => {
+      if (err) throw err;
+      callback(data);
+    });
+  },
+
+  insertOne: (table, columns, values, callback) => {
+    const queryString = `INSERT INTO ${table}(${columns.join()}) VALUES ?`;
+    connection.query(queryString, [
+      values
+    ], (err, data) => {
+      if (err) throw err;
+      callback(data)
+    });
+  },
+
+  updateOne: (table, updateVals, conditions, callback) => {
+    const queryString = `UPDATE ${table} SET ${objToSql(updateVals)} WHERE ${conditions}`;
+    connection.query(queryString, (err, data) => {
+      if (err) throw err;
+      callback(data)
+    })
+  },
+
+  delete: (table, conditions, callback) => {
+    const queryString = `DELETE FROM ${table} WHERE ${conditions}`;
+    connection.query(queryString, (err, data) => {
+      if (err) throw err;
+      callback(data)
+    })
+  }
+};
