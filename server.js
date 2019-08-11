a3842529c72cf5fe7039f5dbdba2a87c0c3d17fb
Aaron <aaron@keisler.dev>
2019-08-10 22:56:55 -0400

    add server functionality

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('This is a test.');
-});
+const routes = require("./controllers/burger_controller")
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+
+app.set("view engine", "ejs")
+
+app.use(routes)
 
 app.listen(PORT, () => {
-  console.log('Example app listening on port: ${PORT}!');
+  console.log(`Example app listening on port: ${PORT}!`);
 });
