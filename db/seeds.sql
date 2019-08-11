c483a131a786bd191baa7c44980d77b7233784ed
Aaron <aaron@keisler.dev>
2019-08-10 22:56:07 -0400

    feature: add schema and dummy data

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+-- Insert rows into table 'TableName'
+INSERT INTO burgers
+( -- columns to insert data into
+ burger_name
+)
+VALUES
+( -- first row: values for the columns in the list above
+ "Shake Shack"
+),
+( -- second row: values for the columns in the list above
+ "Five Guys"
+),
+(
+  "Duke's Grocery"
+);
