cda9de9db3fb58d824f6786cd8bccfe9c3675f32
Aaron <aaron@keisler.dev>
2019-08-16 02:16:29 -0400

    add functions to public js file

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const pic = document.querySelector("#burgerPic");
+  document.querySelectorAll(".eat").forEach(button => {
+    button.addEventListener("click", handleEat);
+  });
 });
+
+function pics() {
+  const pic = document.querySelector("#burgerPic");
+  fetch("");
+}
+
+function handleEat(e) {
+  const data = {
+    devoured: e.target.closest("tr").children[1].textContent === "Nope" ? 1 : 0
+  };
+  fetch(`/burgers/${e.target.dataset.id}/eat`, {
+    method: "PUT",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    },
+    redirect: "follow",
+  })
+    .then(res => res.text())
+    .then(response => console.log("Success:", JSON.stringify(response)))
+    .catch(error => console.error("Error:", error));
+}
