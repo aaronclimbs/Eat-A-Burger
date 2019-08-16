8af4d69003ff034ee1a2a7a7246793161da0816e
Aaron Keisler <amkeisler@gmail.com>
2019-08-15 20:07:28 -0400

    add burger model

--- models/burger.js
+++ models/burger.js
@@ -1,26 +1,32 @@
-const orm = require('../config/orm');
+const orm = require("../config/orm");
 
 module.exports = {
   all: function(callback) {
     orm.selectAll("burgers", function(res) {
       callback(res);
-    })
+    });
+  },
+
+  findOne: function(condition, callback) {
+    orm.selectOne("burgers", condition, function(res) {
+      callback(res);
+    });
   },
 
   create: function(cols, vals, callback) {
     orm.insertOne("burgers", cols, vals, function(res) {
       callback(res);
-    })
+    });
   },
 
   update: function(objColVals, condition, callback) {
     orm.updateOne("burgers", objColVals, condition, function(res) {
       callback(res);
-    })
+    });
   },
   delete: function(condition, callback) {
     orm.delete(condition, function(res) {
       callback(res);
-    })
-  },
-}
+    });
+  }
+};
